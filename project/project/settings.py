1e9362d98243802388cc9d7a1af1faf9241ae86c
Aloysius Thomas <aloysiusthomas@xeoscript.com>
2021-01-24 22:38:42 +0530

    home

--- project/project/settings.py
+++ project/project/settings.py
@@ -114,4 +114,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static-root"
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'static/media'
