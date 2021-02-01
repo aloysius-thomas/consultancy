056b49c474e8efba63947c01f00306e3eb921b9b
Aloysius Thomas <aloysiusthomas@xeoscript.com>
2021-02-01 21:53:50 +0530

    fix

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants as message_constants
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
